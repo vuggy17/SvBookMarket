8ca02b33e1e264ffaeb9e3f33552ec7679c9bdce
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-04-05 16:15:14 +0700

    complete Title and back button

--- app/src/main/java/com/example/svbookmarket/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/RegisterActivity.kt
@@ -2,12 +2,20 @@ package com.example.svbookmarket.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.widget.ImageViewCompat
 import com.example.svbookmarket.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val imgBackArrow: AppCompatImageView = findViewById(R.id.imgBackArrow)
+        imgBackArrow.setOnClickListener {
+            onBackPressed()
+        }
 
     }
 }
