c0cb4bcbbcc9c4cf8cabd413f607d09076695308
Tân Phạm Minh <19520261@ms.uit.edu.vn>
2021-04-05 10:47:24 +0700

    Done splash screen without font

--- app/src/main/java/com/example/svbookmarket/activities/SplashActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.svbookmarket.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.svbookmarket.R
@@ -11,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        // set full screen
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -21,6 +23,15 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+        // set time to pass this activity
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+                {
+                    startActivity(Intent(baseContext,MainActivity::class.java))
+                    finish()
+                },
+                3000 // set time
+        )
 
     }
 }
