759662b6845e13611c32f2e8646e5e4fc77bde1d
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-04-10 14:30:37 +0700

    fix material error

--- app/src/main/java/com/example/svbookmarket/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.svbookmarket.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.svbookmarket.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
