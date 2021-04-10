29783e8faa0d3523f37cd6ba8fc1f82efa3f0ce9
vuggy17 <vuggy17@gmail.com>
2021-04-10 19:31:05 +0700

    checkout card

--- app/src/main/java/com/example/svbookmarket/activities/CheckoutActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/CheckoutActivity.kt
@@ -1,13 +1,14 @@
 package com.example.svbookmarket.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.svbookmarket.R
 import com.example.svbookmarket.activities.adapter.CheckoutAdapter
 import com.example.svbookmarket.activities.data.DataSource
+import kotlinx.android.synthetic.main.card_checkout.*
 
-class MainActivity : AppCompatActivity() {
+class CheckoutActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // set activity to display here
@@ -18,9 +19,11 @@ class MainActivity : AppCompatActivity() {
 //            card.setChecked(!card.isChecked)
 //            true
 //        }
-        val myDataset = DataSource().loadCheckoutCard().toMutableList()
+
+        val myDataset =DataSource().loadCheckoutCard()
         val recyclerView  = findViewById<RecyclerView>(R.id.rc_checkout)
         recyclerView.adapter = CheckoutAdapter(this,myDataset)
         recyclerView.setHasFixedSize(true)
     }
+
 }
---
+++ app/src/main/java/com/example/svbookmarket/activities/adapter/CheckoutAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.svbookmarket.activities.adapter
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.svbookmarket.R
+import com.example.svbookmarket.activities.model.CheckoutCard
+import com.google.android.material.card.MaterialCardView
+
+class CheckoutAdapter(private val context: Context, private val items: MutableList<CheckoutCard>) :
+    RecyclerView.Adapter<CheckoutAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.card_checkout, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = items[position]
+        holder.author.text = context.resources.getString(item.author)
+//        holder.cover.text = context.resources.getString(item.cover)
+        holder.bookname.text = context.resources.getString(item.name)
+        holder.price.text = context.resources.getString(item.price)
+        holder.cartNumber.text = context.resources.getString(item.number)
+        //check option
+        // TODO: 10/04/2021 sủa lỗi bấm 2 lần mới nhận 
+        holder.itemView.setOnClickListener {
+            it.setOnClickListener {
+                (it as MaterialCardView).setChecked(!it.isChecked)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val author: TextView = view.findViewById(R.id.tv_author)
+        val price: TextView = view.findViewById(R.id.tv_price)
+
+        //    val cover:TextView = view.findViewById(R.id.img_cover)
+        val bookname: TextView = view.findViewById(R.id.tv_bookname)
+        val cartNumber: TextView = view.findViewById(R.id.tv_cartnumber)
+
+
+    }
+
+}
