327b161582a99ee2b051cf6115e80620e548e5b4
vuggy17 <vuggy17@gmail.com>
2021-04-10 16:21:56 +0700

    add kotlin android extension

--- app/src/main/java/com/example/svbookmarket/activities/Base.kt
+++ app/src/main/java/com/example/svbookmarket/activities/Base.kt
@@ -1,9 +1,5 @@
 package com.example.svbookmarket.activities
 
-import android.view.View
 
 open class Base {
-    public fun setCardSelection(){
-
-    }
 }
--- app/src/main/java/com/example/svbookmarket/activities/MainActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.svbookmarket.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.svbookmarket.R
+import kotlinx.android.synthetic.main.card_checkout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // set activity to display here
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.card_checkout)
+
+//        //tam thoi de funtion nay o day
+//        card.setOnClickListener{
+//            card.setChecked(!card.isChecked)
+//            true
+//        }
     }
 }
