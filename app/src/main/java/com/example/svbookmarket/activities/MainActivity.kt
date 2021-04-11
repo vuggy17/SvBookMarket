7899a9a77345197bfd0e194992dc173b0a29ca0f
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-04-11 16:05:16 +0700

    done Edit Profile activity.

--- app/src/main/java/com/example/svbookmarket/activities/MainActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // set activity to display here
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_edit_profile)
     }
 }
--- app/src/main/java/com/example/svbookmarket/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/RegisterActivity.kt
@@ -2,15 +2,12 @@ package com.example.svbookmarket.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.core.widget.ImageViewCompat
 import com.example.svbookmarket.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_edit_profile)
 
 //        val imgBackArrow: AppCompatImageView = findViewById(R.id.imgBackArrow)
 //        imgBackArrow.setOnClickListener {
