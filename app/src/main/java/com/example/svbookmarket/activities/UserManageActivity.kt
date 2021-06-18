bb68835f536a81989eb7093f67fcb6a9b9d23889
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-06-18 20:01:10 +0700

    done user top bar

--- app/src/main/java/com/example/svbookmarket/activities/UserManageActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/UserManageActivity.kt
@@ -3,10 +3,21 @@ package com.example.svbookmarket.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.svbookmarket.R
+import com.example.svbookmarket.databinding.ActivityUserManageBinding
 
 class UserManageActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityUserManageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_manage)
+        binding = ActivityUserManageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setButtonBack()
+    }
+
+    private fun setButtonBack(){
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
