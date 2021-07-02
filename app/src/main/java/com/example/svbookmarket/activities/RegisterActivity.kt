f77d87435404693eebe85e696377901026d2bad8
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-07-03 00:17:50 +0700

    commmit

--- app/src/main/java/com/example/svbookmarket/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/svbookmarket/activities/RegisterActivity.kt
@@ -118,8 +118,6 @@ class RegisterActivity : AppCompatActivity() {
         }.addOnFailureListener { e ->
             Toast.makeText(this, "Register Fail$e", Toast.LENGTH_SHORT).show()
         }
-        val dbReferenceRecentAccountUserAddressAccount = dbReference.document(appAccount.email).collection("userDeliverAddresses")
-        dbReferenceRecentAccountUserAddressAccount.document().set(UserDeliverAddress("",user.fullName, user.phoneNumber, user.addressLane, user.district,user.city, true))
         finish()
         startActivity(Intent(baseContext,LoginActivity::class.java))
     }
